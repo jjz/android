3aeefc17a930b903b56c80cb33edab3b4e2896fb
jjz <woaf1003@gmail.com>
2016-06-16 21:47:18 +0800

    upgrade

--- experimental/app/src/main/jni/experiment.c
+++ experimental/app/src/main/jni/experiment.c
@@ -2,5 +2,7 @@
 
 JNIEXPORT jstring JNICALL
 Java_com_jjz_NativeUtil_firstNative(JNIEnv *env, jclass type) {
-    return (*env)->NewStringUTF(env, "is first native");
+    char chars[] = "i am test";
+    int b=1;
+    return (*env)->NewStringUTF(env, chars);
 }
