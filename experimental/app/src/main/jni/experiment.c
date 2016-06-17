8028854409b27910617acd11f23432f169c49e8e
jjz <woaf1003@gmail.com>
2016-06-17 21:01:09 +0800

    add libs

--- experimental/app/src/main/jni/experiment.c
+++ experimental/app/src/main/jni/experiment.c
@@ -1,8 +1,12 @@
 #include <jni.h>
 
+//#include "openssl/rand.h"
+
 JNIEXPORT jstring JNICALL
 Java_com_jjz_NativeUtil_firstNative(JNIEnv *env, jclass type) {
     char chars[] = "i am test";
     int b=1;
-    return (*env)->NewStringUTF(env, chars);
+    unsigned char rand_str[128];
+  //  RAND_seed(rand_str,32);
+    return (*env)->NewStringUTF(env, rand_str);
 }
