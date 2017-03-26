128942f98600e9cae7021bd16dcbb138a6649d9b
jjz <woaf1003@gmail.com>
2017-03-27 06:36:32 +0800

    upgrade gradle

--- Jni/app/src/main/java/com/example/jjz/jni/HelloJni.java
+++ Jni/app/src/main/java/com/example/jjz/jni/HelloJni.java
@@ -2,9 +2,8 @@ package com.example.jjz.jni;
 
 public class HelloJni {
     static {
+        //加载.so类库，加载的名称去掉lib
         System.loadLibrary("hello");
     }
-
     public native int sayHello(int a);
-
 }
