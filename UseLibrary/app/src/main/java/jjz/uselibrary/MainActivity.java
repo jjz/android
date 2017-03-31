c3edeed37b42593377cb3e08656e320ce58275fd
jjz <woaf1003@gmail.com>
2017-03-31 09:31:05 +0800

    dispaly text

--- UseLibrary/app/src/main/java/jjz/uselibrary/MainActivity.java
+++ UseLibrary/app/src/main/java/jjz/uselibrary/MainActivity.java
@@ -1,7 +1,9 @@
 package jjz.uselibrary;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NativeUtil.callLogFromJni();
+        TextView tv = (TextView) findViewById(R.id.tv);
+        TextView tv2 = (TextView) findViewById(R.id.tv_2);
+        tv2.setText(Base64.encodeToString(NativeUtil.getRandom(), Base64.DEFAULT));
     }
 }
